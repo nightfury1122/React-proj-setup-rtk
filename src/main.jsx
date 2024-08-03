e0f731f01f89ab17c070583db3198668e30fa3a1
Hassan5398 <hassan@we-over-i.com>
2024-08-03 17:31:05 +0500

    provider added for the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { Store } from './app/store.js'
 
+// setting up the providor and passing the store in it as a prop so that the store is provided to the compelete react application 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={Store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
