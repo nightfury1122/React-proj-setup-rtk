2db3827816132270b3073aa04f468fe0caedbd09
Hassan5398 <hassan@we-over-i.com>
2024-08-04 14:18:17 +0500

    data being fetched from an api

--- src/app/features/user/userView.jsx
+++ src/app/features/user/userView.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchUsers } from './userSlice'
+
+
 
 export const UserView = () => {
-  const userList = useSelector((state)=>state.user.users)
+
+  const dispatch = useDispatch()
+
+  // Passing an empty dependency array because we only want this to run when the component mounts 
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+
+  const userData = useSelector((state) => state.user)
+
   return (
     <div>
-    <h2>List of users - {userList} </h2>
-    <button>Order Icecream</button>
-    <button>Restock Icecream</button>
+      <h2>List of users -  </h2>
+      {userData.loading && <div>
+        Loading ...
+      </div>}
+      {!userData.loading && userData.error ? <div>
+        Error : {userData.error}
+      </div> : null
+      }
+      {
+        !userData.loading && userData.users.length ? (
+          <ul>
+            {
+              userData.users.map(user => (
+                <li key={user}>{user}</li>
+              ))
+            }
+          </ul>
+        ) : null
+      }
     </div>
   )
 }
+
+
